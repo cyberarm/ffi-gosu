32b6fce760612a14802ec8e3de0f9807e85b7c2c
Cyberarm <matthewlikesrobots@gmail.com>
2019-08-26 09:02:29 -0500

    Fleshed Gosu module some more, fixed Window.width=/Window.height= not working

--- lib/gosu.rb
+++ lib/gosu.rb
@@ -10,23 +10,42 @@ module Gosu
   callback :_callback,            [],         :void
   callback :_callback_with_block, [:pointer], :void
 
-  attach_function :fps, :Gosu_fps, [], :long
+  # Argumentless functions that don't need nice pretty argument handling
+  attach_function :fps,          :Gosu_fps,          [], :int
+  attach_function :flush,        :Gosu_flush,        [], :void
+  attach_function :language,     :Gosu_language,     [], :string
+  attach_function :milliseconds, :Gosu_milliseconds, [], :long
 
+  # attach_function :_transform, :Gosu_transform, [:double, :double, :_callback_with_block], :void
   attach_function :_translate, :Gosu_translate, [:double, :double, :_callback_with_block], :void
   attach_function :_rotate,    :Gosu_rotate,    [:double, :double, :double, :_callback_with_block], :void
   attach_function :_scale,     :Gosu_scale,     [:double, :double, :double, :double, :_callback_with_block], :void
   attach_function :_clip_to,   :Gosu_clip_to,   [:double, :double, :double, :double, :_callback_with_block], :void
 
-  attach_function :_gl, :Gosu_gl, [:_callback], :void
+  attach_function :_gl,      :Gosu_gl,    [:_callback],            :void
   attach_function :_render, :Gosu_render, [:_callback_with_block], :pointer
   attach_function :_record, :Gosu_record, [:_callback_with_block], :pointer
 
   attach_function :_button_down, :Gosu_button_down, [:uint32], :bool
+  attach_function :button_id_to_char, :Gosu_button_id_to_char, [:uint32], :string
+  attach_function :button_char_to_id, :Gosu_button_char_to_id, [:string], :uint32
 
   attach_function :_draw_line, :Gosu_draw_line, [:double, :double, :uint32, :double, :double, :uint32, :double, :uint32], :void
   attach_function :_draw_quad, :Gosu_draw_quad, [:double, :double, :uint32, :double, :double, :uint32, :double, :uint32,
                                                  :double, :double, :uint32, :double, :double, :uint32, :double, :uint32], :void
   attach_function :_draw_rect, :Gosu_draw_rect, [:double, :double, :double, :double, :uint32, :double, :uint32],          :void
+
+  attach_function :offset_x,   :Gosu_offset_y,   [:double, :double],                   :double
+  attach_function :offset_y,   :Gosu_offset_y,   [:double, :double],                   :double
+  attach_function :distance,   :Gosu_distance,   [:double, :double, :double, :double], :double
+  attach_function :angle,      :Gosu_angle,      [:double, :double, :double, :double], :double
+  attach_function :angle_diff, :Gosu_angle_diff, [:double, :double],                   :double
+  attach_function :random,     :Gosu_random,     [:double, :double],                   :double
+
+  attach_function :_screen_width,     :Gosu_screen_width,     [:pointer], :uint32
+  attach_function :_screen_height,    :Gosu_screen_height,    [:pointer], :uint32
+  attach_function :_available_width,  :Gosu_available_width,  [:pointer], :uint32
+  attach_function :_available_height, :Gosu_available_height, [:pointer], :uint32
 
   def self.gl(z = nil, &block)
     _gl(block)
@@ -75,5 +94,21 @@ module Gosu
 
   def self.draw_rect(x, y, width, height, c, z = 0, mode = 0xffffffff)
     _draw_rect(x, y, width, height, c, z, mode)
+  end
+
+  def self.available_width(window = nil)
+    _available_width(window)
+  end
+
+  def self.available_height(window = nil)
+    _available_height(window)
+  end
+
+  def self.screen_width(window = nil)
+    _screen_width(window)
+  end
+
+  def self.screen_height(window = nil)
+    _screen_height(window)
   end
 end
--- lib/gosu/window.rb
+++ lib/gosu/window.rb
@@ -3,7 +3,7 @@ module Gosu
     extend FFI::Library
     ffi_lib Gosu::LIBRARY_PATH
 
-    callback :_callback_window_redraw,       [],        :void
+    callback :_callback_window_draw,         [],        :void
     callback :_callback_window_update,       [],        :void
     callback :_callback_window_button_down,  [:uint32], :void
     callback :_callback_window_button_up,    [:uint32], :void
@@ -15,7 +15,7 @@ module Gosu
     attach_function :_create_window,  :Gosu_Window_create,  [:int, :int, :bool, :double, :bool], :pointer
     attach_function :_destroy_window, :Gosu_Window_destroy, [:pointer],                          :void
 
-    attach_function :_window_set_draw,         :Gosu_Window_set_draw,         [:pointer, :_callback_window_redraw],       :void
+    attach_function :_window_set_draw,         :Gosu_Window_set_draw,         [:pointer, :_callback_window_draw],         :void
     attach_function :_window_set_update,       :Gosu_Window_set_update,       [:pointer, :_callback_window_update],       :void
     attach_function :_window_set_button_down,  :Gosu_Window_set_button_down,  [:pointer, :_callback_window_button_down],  :void
     attach_function :_window_set_button_up,    :Gosu_Window_set_button_up,    [:pointer, :_callback_window_button_up],    :void
@@ -24,18 +24,25 @@ module Gosu
     attach_function :_window_set_needs_cursor, :Gosu_Window_set_needs_cursor, [:pointer, :_callback_window_needs_cursor], :void
     attach_function :_window_set_close,        :Gosu_Window_set_close,        [:pointer, :_callback_window_close],        :void
 
-    attach_function :_window_show,              :Gosu_Window_show,              [:pointer],          :void
-    attach_function :_window_close_immediately, :Gosu_Window_close_immediately, [:pointer],          :void
-    attach_function :_window_width,             :Gosu_Window_width,             [:pointer],          :int
-    attach_function :_window_set_width,         :Gosu_Window_set_width,         [:pointer, :int],    :void
-    attach_function :_window_height,            :Gosu_Window_height,            [:pointer],          :int
-    attach_function :_window_set_height,        :Gosu_Window_set_height,        [:pointer, :int],    :void
-    attach_function :_window_mouse_x,           :Gosu_Window_mouse_x,           [:pointer],          :double
-    attach_function :_window_set_mouse_x,       :Gosu_Window_set_mouse_x,       [:pointer, :double],  :void
-    attach_function :_window_mouse_y,           :Gosu_Window_mouse_y,           [:pointer],          :double
-    attach_function :_window_set_mouse_y,       :Gosu_Window_set_mouse_y,       [:pointer, :double],  :void
-    attach_function :_window_caption,           :Gosu_Window_caption,           [:pointer],          :string
-    attach_function :_window_set_caption,       :Gosu_Window_set_caption,       [:pointer, :string], :void
+    # Enable gosu's default button_down fullscreen toggle
+    attach_function :_window_gosu_button_down, :Gosu_Window_gosu_button_down, [:pointer, :uint32], :void
+
+    attach_function :_window_show,                :Gosu_Window_show,                [:pointer],                    :void
+    attach_function :_window_close_immediately,   :Gosu_Window_close_immediately,   [:pointer],                    :void
+    attach_function :_window_width,               :Gosu_Window_width,               [:pointer],                    :int
+    attach_function :_window_set_width,           :Gosu_Window_set_width,           [:pointer, :int],              :void
+    attach_function :_window_height,              :Gosu_Window_height,              [:pointer],                    :int
+    attach_function :_window_set_height,          :Gosu_Window_set_height,          [:pointer, :int],              :void
+    attach_function :_window_mouse_x,             :Gosu_Window_mouse_x,             [:pointer],                    :double
+    attach_function :_window_set_mouse_x,         :Gosu_Window_set_mouse_x,         [:pointer, :double],           :void
+    attach_function :_window_mouse_y,             :Gosu_Window_mouse_y,             [:pointer],                    :double
+    attach_function :_window_set_mouse_y,         :Gosu_Window_set_mouse_y,         [:pointer, :double],           :void
+    attach_function :_window_caption,             :Gosu_Window_caption,             [:pointer],                    :string
+    attach_function :_window_set_caption,         :Gosu_Window_set_caption,         [:pointer, :string],           :void
+    attach_function :_window_update_interval,     :Gosu_Window_update_interval,     [:pointer],                    :double
+    attach_function :_window_set_update_interval, :Gosu_Window_set_update_interval, [:pointer, :double],           :void
+    attach_function :_window_resize,              :Gosu_Window_resize,              [:pointer, :int, :int, :bool], :void
+    attach_function :_window_fullscreen,          :Gosu_Window_fullscreen,          [:pointer],                    :bool
 
     def initialize(width, height, fullscreen: false, update_interval: 16.66666667, resizable: false)
       @__window = _create_window(width, height, fullscreen, update_interval, resizable)
@@ -60,13 +67,18 @@ module Gosu
       _window_set_close(@__window, @__close_proc)
     end
 
+    # Returns FFI pointer of C side Gosu::Window
+    def __pointer
+      @__window
+    end
+
     def update; end
     def draw; end
-    def button_down(id); end
+    def button_down(id); _window_gosu_button_down(@__window, id); end
     def button_up(id); end
     def drop(filename); end
     def needs_redraw?; true; end
-    def needs_cursor?; true; end
+    def needs_cursor?; false; end
     def close; close!; end
 
     def caption
@@ -107,7 +119,7 @@ module Gosu
     end
 
     def width=(int)
-      _window_resize(@__window, int, self.height, self.fullscreen)
+      _window_resize(@__window, int, height, fullscreen?)
     end
 
     def height
@@ -115,7 +127,7 @@ module Gosu
     end
 
     def height=(int)
-      _window_resize(@__window, self.width, int, self.fullscreen)
+      _window_resize(@__window, width, int, fullscreen?)
     end
 
     def mouse_x
