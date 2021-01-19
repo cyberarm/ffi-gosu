12985d417c0d43bdc59ae0d7f19ad2149e99dbae
Cyberarm <matthewlikesrobots@gmail.com>
2021-01-19 10:32:50 -0600

    Add dlls to load path

--- lib/gosu/constants.rb
+++ lib/gosu/constants.rb
@@ -1,8 +1,29 @@
+if RUBY_PLATFORM =~ /mswin$|mingw32|mingw64|win32\-|\-win32/
+  binary_path = File.expand_path("../../../gosu/lib", __dir__)#File.dirname(__FILE__)
+  # 64-bit builds of Windows use "x64-mingw32" as RUBY_PLATFORM
+  binary_path += "64" if RUBY_PLATFORM =~ /^x64-/
+
+  begin
+    # Make DLLs available as shown here:
+    # https://github.com/oneclick/rubyinstaller2/wiki/For-gem-developers
+    require 'ruby_installer'
+    RubyInstaller::Runtime.add_dll_directory(binary_path)
+  rescue LoadError
+    # Add this gem to the PATH on Windows so that bundled DLLs can be found.
+    # When running through Ocra on Windows, we need to be careful to preserve the ENV["PATH"]
+    # encoding (see #385).
+    path_encoding = ENV["PATH"].encoding
+    ENV["PATH"] = "#{binary_path.encode(path_encoding)};#{ENV["PATH"]}"
+  end
+
+  # Add the correct lib directory for the current version of Ruby (major.minor).
+  $LOAD_PATH.unshift File.join(binary_path, RUBY_VERSION[/^\d+.\d+/])
+end
+
 module Gosu
   LIBRARY_PATH = [
     File.expand_path("../../../gosu/build/ffi/libgosu-ffi.so", __dir__),
-    File.expand_path("../../../gosu/lib64/gosu-ffi.dll", __dir__).gsub("/", "\\"),
-    "gosu-ffi"
+	"gosu-ffi"
   ]
 
   extend FFI::Library
