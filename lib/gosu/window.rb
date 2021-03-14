d6975596a8381638d7b188a8e1f76e1a99ebbc65
Julian Raschke <julian@raschke.de>
2021-03-14 14:09:08 +0100

    Add FFI exception translation for Gosu::Window

--- lib/gosu/window.rb
+++ lib/gosu/window.rb
@@ -9,57 +9,57 @@ module Gosu
     callback :_callback_returns_bool,                  [:pointer],             :bool
     callback :_callback_hit_test_returns_unsigned,     [:pointer, :int, :int], :uint32
 
-    attach_function :_create_window,  :Gosu_Window_create,  [:int, :int, :uint32, :double], :pointer
-    attach_function :_destroy_window, :Gosu_Window_destroy, [:pointer],                     :void
+    attach_function :Gosu_Window_create,  [:int, :int, :uint32, :double], :pointer
+    attach_function :Gosu_Window_destroy, [:pointer],                     :void
 
-    attach_function :_window_set_draw,                 :Gosu_Window_set_draw,                 [:pointer, :_callback, :pointer],                           :void
-    attach_function :_window_set_update,               :Gosu_Window_set_update,               [:pointer, :_callback, :pointer],                           :void
-    attach_function :_window_set_button_down,          :Gosu_Window_set_button_down,          [:pointer, :_callback_with_unsigned, :pointer],             :void
-    attach_function :_window_set_button_up,            :Gosu_Window_set_button_up,            [:pointer, :_callback_with_unsigned, :pointer],             :void
-    attach_function :_window_set_gamepad_connected,    :Gosu_Window_set_gamepad_connected,    [:pointer, :_callback_with_unsigned, :pointer],             :void
-    attach_function :_window_set_gamepad_disconnected, :Gosu_Window_set_gamepad_disconnected, [:pointer, :_callback_with_unsigned, :pointer],             :void
-    attach_function :_window_set_drop,                 :Gosu_Window_set_drop,                 [:pointer, :_callback_with_string, :pointer],               :void
-    attach_function :_window_set_needs_redraw,         :Gosu_Window_set_needs_redraw,         [:pointer, :_callback_returns_bool, :pointer],              :void
-    attach_function :_window_set_needs_cursor,         :Gosu_Window_set_needs_cursor,         [:pointer, :_callback_returns_bool, :pointer],              :void
-    #attach_function :_window_set_capture_cursor,       :Gosu_Window_set_capture_cursor,       [:pointer, :_callback_returns_bool, :pointer],              :void
-    #attach_function :_window_set_hit_test,             :Gosu_Window_set_hit_test,             [:pointer, :_callback_hit_test_returns_unsigned, :pointer], :void
-    attach_function :_window_set_close,                :Gosu_Window_set_close,                [:pointer, :_callback, :pointer],                           :void
+    attach_function :Gosu_Window_set_draw,                 [:pointer, :_callback, :pointer],                           :void
+    attach_function :Gosu_Window_set_update,               [:pointer, :_callback, :pointer],                           :void
+    attach_function :Gosu_Window_set_button_down,          [:pointer, :_callback_with_unsigned, :pointer],             :void
+    attach_function :Gosu_Window_set_button_up,            [:pointer, :_callback_with_unsigned, :pointer],             :void
+    attach_function :Gosu_Window_set_gamepad_connected,    [:pointer, :_callback_with_unsigned, :pointer],             :void
+    attach_function :Gosu_Window_set_gamepad_disconnected, [:pointer, :_callback_with_unsigned, :pointer],             :void
+    attach_function :Gosu_Window_set_drop,                 [:pointer, :_callback_with_string, :pointer],               :void
+    attach_function :Gosu_Window_set_needs_redraw,         [:pointer, :_callback_returns_bool, :pointer],              :void
+    attach_function :Gosu_Window_set_needs_cursor,         [:pointer, :_callback_returns_bool, :pointer],              :void
+    #attach_function :Gosu_Window_set_capture_cursor,       [:pointer, :_callback_returns_bool, :pointer],              :void
+    #attach_function :Gosu_Window_set_hit_test,             [:pointer, :_callback_hit_test_returns_unsigned, :pointer], :void
+    attach_function :Gosu_Window_set_close,                [:pointer, :_callback, :pointer],                           :void
 
     # Enable gosu's default button_down fullscreen toggle
-    attach_function :_window_default_button_down, :Gosu_Window_default_button_down, [:pointer, :uint32], :void
+    attach_function :Gosu_Window_default_button_down, [:pointer, :uint32], :void
 
-    attach_function :_window_show,                :Gosu_Window_show,                [:pointer],                    :void
-    attach_function :_window_tick,                :Gosu_Window_tick,                [:pointer],                    :bool
-    attach_function :_window_close_immediately,   :Gosu_Window_close_immediately,   [:pointer],                    :void
+    attach_function :Gosu_Window_show,                [:pointer],                    :void
+    attach_function :Gosu_Window_tick,                [:pointer],                    :bool
+    attach_function :Gosu_Window_close_immediately,   [:pointer],                    :void
 
-    attach_function :_window_width,               :Gosu_Window_width,               [:pointer],                    :int
-    attach_function :_window_set_width,           :Gosu_Window_set_width,           [:pointer, :int],              :void
-    attach_function :_window_height,              :Gosu_Window_height,              [:pointer],                    :int
-    attach_function :_window_set_height,          :Gosu_Window_set_height,          [:pointer, :int],              :void
+    attach_function :Gosu_Window_width,               [:pointer],                    :int
+    attach_function :Gosu_Window_set_width,           [:pointer, :int],              :void
+    attach_function :Gosu_Window_height,              [:pointer],                    :int
+    attach_function :Gosu_Window_set_height,          [:pointer, :int],              :void
 
-    attach_function :_window_mouse_x,             :Gosu_Window_mouse_x,             [:pointer],                    :double
-    attach_function :_window_set_mouse_x,         :Gosu_Window_set_mouse_x,         [:pointer, :double],           :void
-    attach_function :_window_mouse_y,             :Gosu_Window_mouse_y,             [:pointer],                    :double
-    attach_function :_window_set_mouse_y,         :Gosu_Window_set_mouse_y,         [:pointer, :double],           :void
+    attach_function :Gosu_Window_mouse_x,             [:pointer],                    :double
+    attach_function :Gosu_Window_set_mouse_x,         [:pointer, :double],           :void
+    attach_function :Gosu_Window_mouse_y,             [:pointer],                    :double
+    attach_function :Gosu_Window_set_mouse_y,         [:pointer, :double],           :void
 
-    attach_function :_window_caption,             :Gosu_Window_caption,             [:pointer],                    :string
-    attach_function :_window_set_caption,         :Gosu_Window_set_caption,         [:pointer, :string],           :void
-    attach_function :_window_update_interval,     :Gosu_Window_update_interval,     [:pointer],                    :double
-    attach_function :_window_set_update_interval, :Gosu_Window_set_update_interval, [:pointer, :double],           :void
-    attach_function :_window_resize,              :Gosu_Window_resize,              [:pointer, :int, :int, :bool], :void
-    attach_function :_window_is_fullscreen,       :Gosu_Window_is_fullscreen,       [:pointer],                    :bool
-    attach_function :_window_is_resizable,        :Gosu_Window_is_resizable,        [:pointer],                    :bool
-    attach_function :_window_is_borderless,       :Gosu_Window_is_borderless,       [:pointer],                    :bool
-    attach_function :_window_set_fullscreen,      :Gosu_Window_set_fullscreen,      [:pointer, :bool],             :void
-    attach_function :_window_set_resizable,       :Gosu_Window_set_resizable,       [:pointer, :bool],             :void
-    attach_function :_window_set_borderless,      :Gosu_Window_set_borderless,      [:pointer, :bool],             :void
+    attach_function :Gosu_Window_caption,             [:pointer],                    :string
+    attach_function :Gosu_Window_set_caption,         [:pointer, :string],           :void
+    attach_function :Gosu_Window_update_interval,     [:pointer],                    :double
+    attach_function :Gosu_Window_set_update_interval, [:pointer, :double],           :void
+    attach_function :Gosu_Window_resize,              [:pointer, :int, :int, :bool], :void
+    attach_function :Gosu_Window_is_fullscreen,       [:pointer],                    :bool
+    attach_function :Gosu_Window_is_resizable,        [:pointer],                    :bool
+    attach_function :Gosu_Window_is_borderless,       [:pointer],                    :bool
+    attach_function :Gosu_Window_set_fullscreen,      [:pointer, :bool],             :void
+    attach_function :Gosu_Window_set_resizable,       [:pointer, :bool],             :void
+    attach_function :Gosu_Window_set_borderless,      [:pointer, :bool],             :void
 
-    #attach_function :_window_minimize,            :Gosu_Window_minimize,            [:pointer],                    :void
-    #attach_function :_window_restore,             :Gosu_Window_restore,             [:pointer],                    :void
-    #attach_function :_window_maximize,            :Gosu_Window_maximize,            [:pointer],                    :void
+    #attach_function :Gosu_Window_minimize,            [:pointer],                    :void
+    #attach_function :Gosu_Window_restore,             [:pointer],                    :void
+    #attach_function :Gosu_Window_maximize,            [:pointer],                    :void
 
-    attach_function :_window_text_input,          :Gosu_Window_text_input,          [:pointer],                    :pointer
-    attach_function :_window_set_text_input,      :Gosu_Window_set_text_input,      [:pointer, :pointer],          :void
+    attach_function :Gosu_Window_text_input,          [:pointer],                    :pointer
+    attach_function :Gosu_Window_set_text_input,      [:pointer, :pointer],          :void
 
 
     def initialize(width, height, _fullscreen = nil, _update_interval = nil, _resizable = nil, _borderless = nil,
@@ -71,7 +71,8 @@ module Gosu
 
       window_flags = Gosu.window_flags(fullscreen: fullscreen, resizable: resizable, borderless: borderless)
 
-      __window = _create_window(width, height, window_flags, update_interval)
+      __window = Gosu_Window_create(width, height, window_flags, update_interval)
+      Gosu.check_last_error
       @memory_pointer = FFI::AutoPointer.new(__window, Gosu::Window.method(:release))
       @text_input = nil
 
@@ -88,18 +89,30 @@ module Gosu
       @__hit_test_proc                = proc { |data, x, y| protected_hit_test(x, y) }
       @__close_proc                   = proc { |data| protected_close }
 
-      _window_set_update(__pointer, @__update_proc, nil)
-      _window_set_draw(__pointer, @__draw_proc, nil)
-      _window_set_button_down(__pointer, @__button_down_proc, nil)
-      _window_set_button_up(__pointer, @__button_up_proc, nil)
-      _window_set_gamepad_connected(__pointer, @__gamepad_connected_proc, nil)
-      _window_set_gamepad_disconnected(__pointer, @__gamepad_disconnected_proc, nil)
-      _window_set_drop(__pointer, @__drop_proc, nil)
-      _window_set_needs_redraw(__pointer, @__needs_redraw_proc, nil)
-      _window_set_needs_cursor(__pointer, @__needs_cursor_proc, nil)
-      #_window_set_capture_cursor(__pointer, @__capture_cursor_proc, nil)
-      #_window_set_hit_test(__pointer, @__hit_test_proc, nil)
-      _window_set_close(__pointer, @__close_proc, nil)
+      Gosu_Window_set_update(__pointer, @__update_proc, nil)
+      Gosu.check_last_error
+      Gosu_Window_set_draw(__pointer, @__draw_proc, nil)
+      Gosu.check_last_error
+      Gosu_Window_set_button_down(__pointer, @__button_down_proc, nil)
+      Gosu.check_last_error
+      Gosu_Window_set_button_up(__pointer, @__button_up_proc, nil)
+      Gosu.check_last_error
+      Gosu_Window_set_gamepad_connected(__pointer, @__gamepad_connected_proc, nil)
+      Gosu.check_last_error
+      Gosu_Window_set_gamepad_disconnected(__pointer, @__gamepad_disconnected_proc, nil)
+      Gosu.check_last_error
+      Gosu_Window_set_drop(__pointer, @__drop_proc, nil)
+      Gosu.check_last_error
+      Gosu_Window_set_needs_redraw(__pointer, @__needs_redraw_proc, nil)
+      Gosu.check_last_error
+      Gosu_Window_set_needs_cursor(__pointer, @__needs_cursor_proc, nil)
+      Gosu.check_last_error
+      #Gosu_Window_set_capture_cursor(__pointer, @__capture_cursor_proc, nil)
+      #Gosu.check_last_error
+      #Gosu_Window_set_hit_test(__pointer, @__hit_test_proc, nil)
+      #Gosu.check_last_error
+      Gosu_Window_set_close(__pointer, @__close_proc, nil)
+      Gosu.check_last_error
     end
 
     # Returns FFI pointer of C side Gosu::Window
@@ -120,7 +133,8 @@ module Gosu
     end
 
     def button_down(id)
-      _window_default_button_down(__pointer, id)
+      Gosu_Window_default_button_down(__pointer, id)
+      Gosu.check_last_error
     end
 
     def button_up(id)
@@ -156,35 +170,39 @@ module Gosu
     end
 
     def caption
-      _window_caption(__pointer)
+      Gosu_Window_caption(__pointer).tap { Gosu.check_last_error }
     end
 
     def caption=(text)
-      _window_set_caption(__pointer, text)
+      Gosu_Window_set_caption(__pointer, text)
+      Gosu.check_last_error
     end
 
     def fullscreen?
-      _window_is_fullscreen(__pointer)
+      Gosu_Window_is_fullscreen(__pointer).tap { Gosu.check_last_error }
     end
 
     def fullscreen=(boolean)
-      _window_resize(__pointer, width, height, !!boolean)
+      Gosu_Window_resize(__pointer, width, height, !!boolean)
+      Gosu.check_last_error
     end
 
     def resizable?
-      _window_is_resizable(__pointer)
+      Gosu_Window_is_resizable(__pointer).tap { Gosu.check_last_error }
     end
 
     def resizable=(boolean)
-      _window_set_resizable(__pointer, !!boolean)
+      Gosu_Window_set_resizable(__pointer, !!boolean)
+      Gosu.check_last_error
     end
 
     def borderless?
-      _window_is_borderless(__pointer)
+      Gosu_Window_is_borderless(__pointer).tap { Gosu.check_last_error }
     end
 
     def borderless=(boolean)
-      _window_set_borderless(__pointer, !!boolean)
+      Gosu_Window_set_borderless(__pointer, !!boolean)
+      Gosu.check_last_error
     end
 
     def text_input
@@ -197,47 +215,53 @@ module Gosu
       ptr = text_input ? text_input.__pointer : nil
       @text_input = text_input
 
-      _window_set_text_input(__pointer, ptr)
+      Gosu_Window_set_text_input(__pointer, ptr)
+      Gosu.check_last_error
     end
 
     def update_interval
-      _window_update_interval(__pointer)
+      Gosu_Window_update_interval(__pointer).tap { Gosu.check_last_error }
     end
 
     def update_interval=(double)
-      _window_set_update_interval(__pointer, double)
+      Gosu_Window_set_update_interval(__pointer, double)
+      Gosu.check_last_error
     end
 
     def width
-      _window_width(__pointer)
+      Gosu_Window_width(__pointer).tap { Gosu.check_last_error }
     end
 
     def width=(int)
-      _window_resize(__pointer, int, height, fullscreen?)
+      Gosu_Window_resize(__pointer, int, height, fullscreen?)
+      Gosu.check_last_error
     end
 
     def height
-      _window_height(__pointer)
+      Gosu_Window_height(__pointer).tap { Gosu.check_last_error }
     end
 
     def height=(int)
-      _window_resize(__pointer, width, int, fullscreen?)
+      Gosu_Window_resize(__pointer, width, int, fullscreen?)
+      Gosu.check_last_error
     end
 
     def mouse_x
-      _window_mouse_x(__pointer)
+      Gosu_Window_mouse_x(__pointer).tap { Gosu.check_last_error }
     end
 
     def mouse_x=(double)
-      _window_set_mouse_x(__pointer, double)
+      Gosu_Window_set_mouse_x(__pointer, double)
+      Gosu.check_last_error
     end
 
     def mouse_y
-      _window_mouse_y(__pointer)
+      Gosu_Window_mouse_y(__pointer).tap { Gosu.check_last_error }
     end
 
     def mouse_y=(double)
-      _window_set_mouse_y(__pointer, double)
+      Gosu_Window_set_mouse_y(__pointer, double)
+      Gosu.check_last_error
     end
 
     def set_mouse_position(x, y)
@@ -246,33 +270,40 @@ module Gosu
     end
 
     def show
-      _window_show(__pointer)
+      Gosu_Window_show(__pointer)
 
       raise @__exception if defined?(@__exception)
+
+      Gosu.check_last_error
     end
 
     def tick
-      _window_tick(__pointer)
+      Gosu_Window_tick(__pointer).tap { Gosu.check_last_error }
     end
 
     def minimize
-      _window_minimize(__pointer)
+      Gosu_Window_minimize(__pointer)
+      Gosu.check_last_error
     end
 
     def restore
-      _window_restore(__pointer)
+      Gosu_Window_restore(__pointer)
+      Gosu.check_last_error
     end
 
     def maximize
-      _window_maximize(__pointer)
+      Gosu_Window_maximize(__pointer)
+      Gosu.check_last_error
     end
 
     def close!
-      _window_close_immediately(__pointer)
+      Gosu_Window_close_immediately(__pointer)
+      Gosu.check_last_error
     end
 
     def self.release(pointer)
-      _destroy_window(pointer)
+      Gosu_Window_destroy(pointer)
+      Gosu.check_last_error
     end
   end
 end
